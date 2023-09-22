d0b827c2de4c2746a304836a189665d1b6ed9b5f
Ponsiano De Loor <ponsianodeloor@gmail.com>
2023-09-21 23:58:35 -0500

    Update app.component.ts
    importar los servicios, declarar su tipo, asignarlos al constructor y crear el formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from "@angular/forms";
 
+import { PersonsService } from "./services/person/persons.service";
+import { ClientsService } from "./services/client/clients.service";
+import { AccountTypesService } from "./services/accountType/account-types.service";
+import { AccountsService } from "./services/account/accounts.service";
+import { TransactionsService } from "./services/transaction/transactions.service";
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'frontendBancoPichinchaAngular';
+  personForm: FormGroup;
+  persons: any;
+  clients: any;
+  accountTypes: any;
+  accounts: any;
+  transactions: any;
 
-  constructor(public fb:FormBuilder) {
-
+  constructor(
+    public fb:FormBuilder,
+    public personsService: PersonsService,
+    public clientsService: ClientsService,
+    public accountTypesService: AccountTypesService,
+    public accountsService: AccountsService,
+    public transactionsService: TransactionsService,
+  ) {
+    this.personForm = this.fb.group({ } );
   }
 
   ngOnInit() {
-
+    this.personForm = this.fb.group({
+      identification: [''],
+      name: [''],
+      age: [''],
+      address: [''],
+      phone: [''],
+    } );
   }
 
 }
